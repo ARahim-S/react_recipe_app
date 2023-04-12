8e8e51b9c46b05120841819a0a6e4aee81b95b24
ARahim-S <arahim.s@yandex.com>
2023-04-12 14:36:14 +0300

    keep

--- src/pages/home/RecipeCardComponent.jsx
+++ src/pages/home/RecipeCardComponent.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { Button, RecipeCard, RecipeHeader, RecipeImg } from "./HomeStyles";
 
 const RecipeCardComponent = ({ item }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/details", { state: { item } });
+  };
   return (
     <RecipeCard>
       <RecipeHeader>{item.label}</RecipeHeader>
       <RecipeImg src={item.image} alt={`${item.label}'s image`} />
-      <Button>Details</Button>
+      <Button onClick={handleClick}>Details</Button>
     </RecipeCard>
   );
 };
